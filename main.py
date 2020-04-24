f27adbd5081b9cd58497281221f38ae9a61a443a
Batuhan ERALP <batuhaneralp@hotmail.com>
2020-04-24 20:16:08 +0300

    Integer Change

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from email_sender import postEmail
 def main():
     print("1 - Inbox")
     print("2 - New")
-    q = int(input("Seçim = "))
-    if q == 1:
+    q = input("Seçim = ")
+    if q == "1":
         getEmail()
-    elif q == 2:
+    elif q == "2":
         postEmail()
     else:
         print("Yanlış tercih")
