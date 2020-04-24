fea4e3d173c5a8996f4bda46f344018f30ccb777
Batuhan ERALP <batuhaneralp@hotmail.com>
2020-04-24 20:20:28 +0300

    Emoji Init

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from email_receiver import getEmail
 from email_sender import postEmail
 
 
+def emoji():
+    print(":-D"
+
+
 def main():
     print("1 - Inbox")
     print("2 - New")
-    q = input("Seçim = ")
+    q=input("Seçim = ")
     if q == "1":
         getEmail()
     elif q == "2":
@@ -15,3 +19,4 @@ def main():
 
 
 main()
+emoji()
